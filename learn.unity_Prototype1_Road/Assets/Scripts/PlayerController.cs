b5c919ce2be1ee28f4c35612431dbabe0ef3661c
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-10-28 08:42:28 +0300

    control speed from the inspector

--- learn.unity_Prototype1_Road/Assets/Scripts/PlayerController.cs
+++ learn.unity_Prototype1_Road/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //transform.Translate(0, 0, 0.05f);
-        transform.Translate(Vector3.forward * Time.deltaTime * 20); //more readable
+        //transform.Translate(Vector3.forward * Time.deltaTime * 20); //It's more readable than 0,0,1.------ forward.Time.deltaTime * 20 is mean "MOVE FORWARD 20 METER PER SECOND !!!".
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);  //Now we can change the speed from the inspector.
     }
 }
