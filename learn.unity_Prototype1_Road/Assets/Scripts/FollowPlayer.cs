6d75c2688360996a40b7bb4d9edd6d378d7ae82a
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-10-28 12:55:09 +0300

    added horizontal move to player

--- learn.unity_Prototype1_Road/Assets/Scripts/FollowPlayer.cs
+++ learn.unity_Prototype1_Road/Assets/Scripts/FollowPlayer.cs
@@ -19,7 +19,13 @@ public class FollowPlayer : MonoBehaviour
     {
         //transform.position = player.transform.position; //Set the camera's position to the vehicle's position.
         //transform.position = player.transform.position + new Vector3(0, 10, -18); //Offset the camera behind the player by adding to the player's position BY usin' a "new" Vector3 that even doesn't exist yet.
-        transform.position = player.transform.position + offset; //It's better isn't it ;)
+        //transform.position = player.transform.position + offset; //It's better isn't it ;)
 
     }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset; //To make camera's move much smooth.
+    }
+
 }
--- learn.unity_Prototype1_Road/Assets/Scripts/PlayerController.cs
+++ learn.unity_Prototype1_Road/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 0;
+    public float turnSpeed = 0; // for horizontal moving
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(0, 0, 0.05f);
-        //transform.Translate(Vector3.forward * Time.deltaTime * 20); //It's more readable than 0,0,1.------ forward.Time.deltaTime * 20 is mean "MOVE FORWARD 20 METER PER SECOND !!!".
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);  //Now we can change the speed from the inspector.
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        //transform.Translate(0, 0, 0.05f); // 1
+        //transform.Translate(Vector3.forward * Time.deltaTime * 20); // 2 It's more readable than 0,0,1.------ forward.Time.deltaTime * 20 is mean "MOVE FORWARD 20 METER PER SECOND !!!".
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);  // 3 Now we can change the speed from the inspector.
+
+
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);// 1 for Horizontal move
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);// 2 controll player from keyboard
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput); // 3 Make vehicle rotate instead of slide
+
     }
 }
